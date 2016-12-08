5ea11d390720e9e5583e90871660578e50f8ec65
Gunnar Radzom <rad@Gunnars-MacBook-Pro.local>
2016-12-08 14:42:19 +0100

    Update 2016-12-08T13:42:17.694Z

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import cv from './cv';
-
-import Profile from './components/profile';
-
-ReactDOM.render(<Profile data={cv} />, document.getElementById('app'));
